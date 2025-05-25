e7f9d9a26fac506bc8292684f11112ef46bb9efa
puttipongpn <fampoom1@gmail.com>
2025-05-25 18:19:52 +0700

    refactor: remove unused imports from components and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import ProfileSelectorIndex from './pages/ProfileSelectorIndex';
 import LoginPage from './pages/LoginPage';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <Routes>
