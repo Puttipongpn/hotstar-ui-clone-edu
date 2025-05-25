fbce5911e2afc70e279f541e33877ace37a84665
puttipongpn <fampoom1@gmail.com>
2025-05-25 18:36:44 +0700

    fix: update route path for ProfileSelectorIndex component

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
-      <Route path="/profile-selector" element={<ProfileSelectorIndex />} />
+      <Route path="/select-profile" element={<ProfileSelectorIndex />} />
     </Routes>
   )
 }
